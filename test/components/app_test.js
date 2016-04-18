dad6d550503ba9f5f49a9aa45696bab768a90104
wduntak <wduntak@gmail.com>
2016-04-18 08:18:01 -0400

    Added tests to project

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,29 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// Use 'describe' to group together similar tests
+describe('App Component', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+	// Use 'it' to test a single attribute of a target
+	it('shows the the correct text', () => {
+		
+		// create an instance of App
+		const component = renderComponent(App);
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
+		// Use 'expect' to make an 'assertion' about a target
+		expect(component).to.contain('React simple starter');	
+
+	});
+
+	it('should be a object', () => {
+		
+		// create an instance of App
+		const component = renderComponent(App);
+
+		// Use 'expect' to make an 'assertion' about a target
+		expect(component).to.be.a('object');	
+
+	});
+
 });
+
