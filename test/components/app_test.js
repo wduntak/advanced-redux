a20f60f8ef928ba37a8f0cdbe5f2a7d1495eec68
wduntak <wduntak@gmail.com>
2016-04-24 09:57:15 -0400

    Added tests to actions and reducers

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,29 +1,19 @@
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-// Use 'describe' to group together similar tests
 describe('App Component', () => {
+	let component;
 
-	// Use 'it' to test a single attribute of a target
-	it('shows the the correct text', () => {
-		
-		// create an instance of App
-		const component = renderComponent(App);
-
-		// Use 'expect' to make an 'assertion' about a target
-		expect(component).to.contain('React simple starter');	
-
+	beforeEach(() => {
+		component = renderComponent(App);
 	});
 
-	it('should be a object', () => {
-		
-		// create an instance of App
-		const component = renderComponent(App);
-
-		// Use 'expect' to make an 'assertion' about a target
-		expect(component).to.be.a('object');	
-
+	it('shows a comment box', () => {
+		expect(component.find('.comment-box')).to.exist
 	});
 
+	it('shows a comment list', () => {
+		expect(component.find('.comment-list')).to.exist
+	});
 });
 
